e041c081b5c161903ecfcb6ae96c88b06aace2f9
Nolazco <carlos_sceptile@hotmail.com>
2024-04-23 09:00:47 -0600

    feat: tourism entity created

---
+++ src/Entity/Turismo.php
@@ -0,0 +1,61 @@
+<?php 
+
+namespace App\Entity;
+
+use Lib\Storage\Annotations\Column;
+use Lib\Storage\Traits\ColumnHydrate;
+use Lib\Storage\Traits\AnnotationMappings;
+use App\Model\TurismoRepo;
+use Lib\Storage\AbstractEntity;
+use Lib\Storage\Annotations\Table;
+
+#[Table('turismo', TurismoRepo::class)]
+class Turismo extends AbstractEntity{
+    use AnnotationMappings;
+    use ColumnHydrate;
+
+    #[Column('id')]
+    public int $id;
+
+    #[Column('name')]
+    public string $name;
+
+    #[Column('description')]
+    public string $description;
+
+    #[Column('location')]
+    public string $location;
+
+    public function getId(): int{
+        return $this->id;
+    }
+
+    public function setName(string $name): static{
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getName(): string{
+        return $this->name;
+    }
+
+    public function setDesc(string $description): static{
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getDesc(): string{
+        return $this->description;
+    }
+
+    public function setLocation(string $location): static{
+        $this->location = $location;
+        return $this;
+    }
+
+    public function getLocation(): string{
+        return $this->location;
+    }
+}
+
+?>
