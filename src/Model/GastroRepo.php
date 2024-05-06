cf67946872267d9ebf860e939cd623c9e430b3b3
Nolazco <carlos_sceptile@hotmail.com>
2024-05-05 21:31:19 -0600

    feat: get last three gastronomy posts

--- src/Model/GastroRepo.php
+++ src/Model/GastroRepo.php
@@ -1,9 +1,16 @@
 <?php 
+
 namespace App\Model;
 
 use Lib\Storage\AbstractModel;
 
 class GastroRepo extends AbstractModel{
+    public function getLastThree(): ?array{
+        $data = $this->queryBind(
+            "SELECT * FROM {$this->getTable()} ORDER BY id DESC LIMIT 3"
+        );
 
+        //dd($data);
+        return $data ?? null;
+    }
 }
-?>
