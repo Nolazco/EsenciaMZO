a4bf5917ec0fd0b86ad0d1a9e03c84c926f0acb3
Nolazco <carlos_sceptile@hotmail.com>
2024-05-05 21:34:41 -0600

    feat: get users by email

--- src/Model/UserRepo.php
+++ src/Model/UserRepo.php
@@ -1,10 +1,17 @@
 <?php 
 namespace App\Model;
 
+use App\Entity\User;
 use Lib\Storage\AbstractModel;
 
 class UserRepo extends AbstractModel{
+    public function getByEmail(string $email): ?User{
+        $entity = new $this->entity();
+        $entity->email = $email;
 
+        $query = "SELECT * FROM {$this->getTable()} WHERE email = :email";
+        $data = $this->queryBind($query, $entity, $entity->includeMapping(['email']));
+
+        return $data[0] ?? null;
+    }
 }
-
-?>
