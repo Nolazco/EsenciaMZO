e8f91767e83174f0a6059d4b7a6cd116a1c6a467
Nolazco <carlos_sceptile@hotmail.com>
2024-05-05 21:34:25 -0600

    feat: get last three tourism posts

--- src/Model/TurismoRepo.php
+++ src/Model/TurismoRepo.php
@@ -4,6 +4,11 @@ namespace App\Model;
 use Lib\Storage\AbstractModel;
 
 class TurismoRepo extends AbstractModel{
-    
+    public function getLastThree(): ?array{
+        $data = $this->queryBind(
+            "SELECT * FROM {$this->getTable()} ORDER BY id DESC LIMIT 3"
+        );
+
+        return $data ?? null;
+    }
 }
-?>
